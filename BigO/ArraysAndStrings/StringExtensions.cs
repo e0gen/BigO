add329a49fbd004573e41a317e88a2ef8b2b1f7c
Nevzorov Evgeny <eugen.nevzorov@gmail.com>
2022-08-29 13:05:50 +0300

    isUnique algo

--- BigO/ArraysAndStrings/StringExtensions.cs
+++ BigO/ArraysAndStrings/StringExtensions.cs
@@ -2,9 +2,20 @@
 {
     public static class StringExtensions
     {
+        /// <summary>
+        /// Time Complexity is O(N)
+        /// </summary>
         public static bool IsUnique(this string s)
-        {
-            return true;
+        { 
+            HashSet<char> chars = new();
+            //O(N)
+            for (int i = 0; i < s.Length; i++)
+            {
+                //O(1)
+                chars.Add(s[i]);
+            }
+            //O(1)
+            return chars.Count == s.Length;
         }
     }
 }
